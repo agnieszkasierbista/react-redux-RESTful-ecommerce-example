bec60e7d4528db84144f5814dbcf01fe813a56f0
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-09-26 22:55:15 +0200

    Test currencySwitcherReducer

--- src/reducers.test.ts
+++ src/reducers.test.ts
@@ -1,10 +1,60 @@
 import {currencySwitcherReducer} from './reducers';
-import {toggleIsExtended} from './actions';
+import {initSuccess, setCurrentCurrency, toggleIsExtended} from './actions';
+import {Currency, Product} from './types';
+import {Category} from './components/CategoryTabs/CategoryTabs.types';
+
+const productA: Product = {
+  name: 'a',
+  id: 'b',
+  prices: [],
+  category: 'c',
+  gallery: ['d'],
+  inStock: true,
+  brand: 'z'
+};
+
+const categoryA: Category = {
+  products: [productA], name: ''
+};
+
+const currencyA: Currency = {symbol: '$', label: 'USD'};
+
+const preloadedState = {isExtended: false, currencies: [], currentCurrency: {symbol: '', label: ''}};
 
 describe('reducers', () => {
   describe('currencySwitcher', () => {
-    it('abc', () => {
-      expect(currencySwitcherReducer({}, toggleIsExtended())).toEqual({});
+    it('should allow for toggling isExtended', () => {
+      expect(currencySwitcherReducer({isExtended: false}, toggleIsExtended())).toEqual({
+        isExtended: true
+      });
+    });
+
+    it('should set currencies and currentCurrency', () => {
+      expect(currencySwitcherReducer(
+        preloadedState,
+        initSuccess([
+          {
+            categories: [categoryA],
+            currencies: [currencyA],
+            pathName: 'a'
+          }
+        ], 'a'))
+      ).toEqual({
+        isExtended: false,
+        currencies: [currencyA],
+        currentCurrency: currencyA
+      });
+    });
+
+    it('should set currentCurrency', () => {
+      expect(currencySwitcherReducer(preloadedState, setCurrentCurrency(currencyA))).toEqual({
+        currencies: [],
+        currentCurrency: {
+          label: 'USD',
+          symbol: '$'
+        },
+        isExtended: false
+      });
     });
   });
 });
